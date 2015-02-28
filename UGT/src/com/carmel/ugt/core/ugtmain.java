309bb49e8b7b1d961fc8eefe8fe7c6200e377f4a
Dilip Khandekar <dilipkhandekar@yahoo.com>
2015-02-27 18:17:39 -0800

    Created UGT java project
    Created the Universal Gesture Translator java project

---
+++ UGT/src/com/carmel/ugt/core/ugtmain.java
@@ -0,0 +1,12 @@
+package com.carmel.ugt.core;
+
+
+public class ugtmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to the Universal Gesture Translator v0.1.0");
+	}
+
+}
