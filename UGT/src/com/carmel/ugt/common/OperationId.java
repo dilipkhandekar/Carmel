9a4289f17bbcb108aab15f715329fc0e251bdab8
Dilip Khandekar <dilipkhandekar@yahoo.com>
2015-03-02 21:44:57 -0800

    Adding OpDispatcher

--- UGT/src/com/carmel/ugt/common/OperationId.java
+++ UGT/src/com/carmel/ugt/common/OperationId.java
@@ -1,5 +1,7 @@
 package com.carmel.ugt.common;
 
 public class OperationId {
-
+	int GetOpId() { return opId; }
+	
+	private int opId;
 }
--- UGT/src/com/carmel/ugt/server/UgtOpDispatcher.java
+++ UGT/src/com/carmel/ugt/server/UgtOpDispatcher.java
@@ -1,5 +1,22 @@
 package com.carmel.ugt.server;
 
 public class UgtOpDispatcher {
+	// Need to add the following data members:
+	// RMI Stuff
+	// Operations Dispatch Table
 
+	void Initialize()
+	{
+		// Initialize RMI stuff
+	}
+	
+	void Start()
+	{
+		// Start dispatch threads, initialize queues, etc.
+	}
+	
+	void Shutdown()
+	{
+		// Stop all the dispatch threads, free up queues, etc.
+	}
 }
