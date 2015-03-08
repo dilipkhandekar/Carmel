8f1a2a0dc3725c983502c5c040633146d04e90cd
VRamani <uberhexx@gmail.com>
2015-03-08 14:27:14 -0700

    UGT dependency modified to Java runtime 7 to have Android compatibility
    Now the Basic Remote control UI should get launched with a scroll
    button.  However since Android doesn't support RMI,, the client code is
    commented out.

--- UxRemoteControl/src/com/carmel/ugt/uxremotecontrol/MainActivity.java
+++ UxRemoteControl/src/com/carmel/ugt/uxremotecontrol/MainActivity.java
@@ -1,11 +1,10 @@
 package com.carmel.ugt.uxremotecontrol;
 
-/*
+
 import com.carmel.ugt.client.UgtClient;
 import com.carmel.ugt.client.UgtOpInvoker;
 import com.carmel.ugt.common.OperationArgs;
 import com.carmel.ugt.common.OperationId;
-*/
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -16,37 +15,38 @@ import android.view.*;
 
 public class MainActivity extends Activity {
 
-	//private UgtClient 		m_client;
-	//private UgtOpInvoker 	m_invoker;
+	private UgtClient 		m_client;
+	private UgtOpInvoker 	m_invoker;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
-
-/*		m_client = new UgtClient();
-
+		
 		try
-		{  	
-	    	String host = "";
-	    	m_invoker = m_client.Connect(host);
+		{ 
+	//		m_client = new UgtClient();
+	//    	String host = "";
+	//    	m_invoker = m_client.Connect(host);
 	
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
         }
 
-        m_client.Disconnect();
-*/
+       // m_client.Disconnect();
+
 	}
 	
 	public void OnClickScroll(View view) 
 	{
-	/*	try
+		try
 		{
-            System.println("OnScrollClicked");
+            System.out.print("OnScrollClicked");
 
-			/*for (int i=0; i < 10; i++)
+/*			for (int i=0; i < 10; i++)
 			{
 				m_invoker.Invoke(OperationId.ID.OpIdScroll, new OperationArgs(10));
 				Thread.sleep(200);
@@ -57,9 +57,9 @@ public class MainActivity extends Activity {
 				m_invoker.Invoke(OperationId.ID.OpIdScroll, new OperationArgs(-10));
 				Thread.sleep(200);
 			}
-        } catch (Exception e) {
+ */       } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
-        }*/
+        }
 	}	
 }
